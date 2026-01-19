18ad9174f5d2b1806f80e2080261821e5d0be06e
Pakura <Alexperezamudio@gmail.com>
2026-01-19 16:12:12 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'konta-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './dashboard.html',
+  './add.html',
+  './detail.html',
+  './calendar.html',
+  './style.css',
+  './app.js',
+  './dashboard.js',
+  './add.js',
+  './detail.js',
+  './calendar.js',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Instalación: Guardamos los archivos en caché
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Fetch: Intentamos servir desde caché, si no, vamos a la red
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
